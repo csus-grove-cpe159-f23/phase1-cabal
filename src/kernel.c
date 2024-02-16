df83034382fc0283bd53620e2d77d25f7a9e88fc
Hannah Nelson <nexec64@gmail.com>
2024-02-15 23:04:48 -0800

    got log levels working properly

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,11 @@
-/**
+/** //f
  * CPE/CSC 159 - Operating System Pragmatics
  * California State University, Sacramento
  *
  * Kernel functions
  */
+//d
+//f include stuff
 #include <spede/flames.h>   // for breakpoint()
 #include <spede/stdarg.h>   // for variable argument functions (va_*)
 #include <spede/stdio.h>    // for printf
@@ -12,51 +14,66 @@
 #include "kernel.h"
 #include "vga.h"
 #include "keyboard.h"
-
+//d
+//f set detault log level
+// this feels like it should be in the header but ok
 #ifndef KERNEL_LOG_LEVEL_DEFAULT
 #define KERNEL_LOG_LEVEL_DEFAULT KERNEL_LOG_LEVEL_TRACE
 #endif
-
-// Current log level
+//d
+//f current log level global valriable
 int kernel_log_level = KERNEL_LOG_LEVEL_DEFAULT;
+//d
 
-/**
- * Initializes any kernel internal data structures and variables
- */
-void kernel_init(void) {
+void kernel_init(void) { //f
+    /**
+     * Initializes any kernel internal data structures and variables
+     */
     // Display a welcome message on the host
     kernel_log_info("Welcome to %s!", OS_NAME);
-
     kernel_log_info("Initializing kernel...");
 }
-
-/**
- * Prints a kernel log message to the host with an error log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_error(char *msg, ...) {
-    // Implement me
+//d
+//f log_level_function_body macro 
+#define log_level_function_body(__LEVEL__NUMBER__, __LEVEL__PRINT__STRING__) {\
+    if (kernel_log_level < KERNEL_LOG_LEVEL_ERROR) {\
+        return;\
+    }\
+    va_list args;\
+    printf(__LEVEL__PRINT__STRING__);\
+    va_start(args, msg);vprintf(msg, args);va_end(args);\
+    printf("\n");\
 }
-
-/**
- * Prints a kernel log message to the host with a warning log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_warn(char *msg, ...) {
-    // Implement me
+//d
+void kernel_log_error(char *msg, ...) { //f
+    /** //f
+     * Prints a kernel log message to the host with an error log level
+     *
+     * @param msg - string format for the message to be displayed
+     * @param ... - variable arguments to pass in to the string format
+     */
+    //d
+    log_level_function_body(KERNEL_LOG_LEVEL_ERROR,"error: ")
 }
-
-/**
- * Prints a kernel log message to the host with an info log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_info(char *msg, ...) {
+//d 
+void kernel_log_warn(char *msg, ...) { //f
+    /** //f
+     * Prints a kernel log message to the host with a warning log level
+     *
+     * @param msg - string format for the message to be displayed
+     * @param ... - variable arguments to pass in to the string format
+     */
+    //d
+    log_level_function_body(KERNEL_LOG_LEVEL_WARN,"warning: ")
+}
+//d
+void kernel_log_info(char *msg, ...) { //f
+    /**
+     * Prints a kernel log message to the host with an info log level
+     *
+     * @param msg - string format for the message to be displayed
+     * @param ... - variable arguments to pass in to the string format
+     */
     // Return if our log level is less than info
     if (kernel_log_level < KERNEL_LOG_LEVEL_INFO) {
         return;
@@ -72,61 +89,73 @@ void kernel_log_info(char *msg, ...) {
     va_start(args, msg);
     vprintf(msg, args);
     va_end(args);
-
     printf("\n");
 }
-
-/**
- * Prints a kernel log message to the host with a debug log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_debug(char *msg, ...) {
-    // Implement me
+//d
+void kernel_log_debug(char *msg, ...) { //f
+    /** //f
+     * Prints a kernel log message to the host with a debug log level
+     *
+     * @param msg - string format for the message to be displayed
+     * @param ... - variable arguments to pass in to the string format
+     */
+    //d
+    log_level_function_body(KERNEL_LOG_LEVEL_DEBUG,"debug: ")
 }
-
-/**
- * Prints a kernel log message to the host with a trace log level
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_log_trace(char *msg, ...) {
-    // Implement me
+//d
+void kernel_log_trace(char *msg, ...) { //f
+    /** //f
+     * Prints a kernel log message to the host with a trace log level
+     *
+     * @param msg - string format for the message to be displayed
+     * @param ... - variable arguments to pass in to the string format
+     */
+    //d
+    log_level_function_body(KERNEL_LOG_LEVEL_TRACE,"trace: ")
 }
-
-/**
- * Triggers a kernel panic that does the following:
- *   - Displays a panic message on the host console
- *   - Triggers a breakpiont (if running through GDB)
- *   - aborts/exits the operating system program
- *
- * @param msg - string format for the message to be displayed
- * @param ... - variable arguments to pass in to the string format
- */
-void kernel_panic(char *msg, ...) {
+//d
+void kernel_panic(char *msg, ...) { //f
+    /** //f
+     * Triggers a kernel panic that does the following:
+     *   - Displays a panic message on the host console
+     *   - Triggers a breakpiont (if running through GDB)
+     *   - aborts/exits the operating system program
+     *
+     * @param msg - string format for the message to be displayed
+     * @param ... - variable arguments to pass in to the string format
+     */
+    //d
+    // Obtain the list of variable arguments
+    va_list args;
+    // Indicate this is an 'info' type of message
+    printf("panic: ");
+    // Pass the message variable arguments to vprintf
+    va_start(args, msg);
+    vprintf(msg, args);
+    va_end(args);
+    printf("\n");
     // Trigger a breakpoint to inspect what caused the panic
     kernel_break();
-
+    //TODO specification PHASE1-PAGE8 specifies that kernel panic must also print to vga
     // Exit since this is fatal
     exit(1);
 }
+//d
 
-/**
- * Returns the current log level
- * @return the kernel log level
- */
-int kernel_get_log_level(void) {
+int kernel_get_log_level(void) { //f
+    /**
+     * Returns the current log level
+     * @return the kernel log level
+     */
     return kernel_log_level;
 }
-
-/**
- * Sets the new log level and returns the value set
- * @param level - the log level to set
- * @return the kernel log level
- */
-int kernel_set_log_level(log_level_t level) {
+//d
+int kernel_set_log_level(log_level_t level) { //f
+    /**
+     * Sets the new log level and returns the value set
+     * @param level - the log level to set
+     * @return the kernel log level
+     */
     int prev_log_level = kernel_log_level;
 
     if (level < KERNEL_LOG_LEVEL_NONE) {
@@ -143,18 +172,18 @@ int kernel_set_log_level(log_level_t level) {
 
     return kernel_log_level;
 }
-
-/**
- * Triggers a breakpoint (if running under GBD)
- */
-void kernel_break(void) {
+//d
+void kernel_break(void) { //f
+    /**
+     * Triggers a breakpoint (if running under GBD)
+     */
     breakpoint();
 }
-
-/**
- * Triggers a kernel command
- */
-void kernel_command(char c) {
+//d
+void kernel_command(char c) { //f TODO
+    /**
+     * Triggers a kernel command
+     */
     static int kernel_escape = 0;
 
     switch (c) {
@@ -170,10 +199,10 @@ void kernel_command(char c) {
             kernel_break();
             break;
 
-        // Add new commands to:
-        //  - Clear the screen (k)
-        //  - Increase the kernel log level (+)
-        //  - Decrease the kernel log level (-)
+            // Add new commands to:
+            //  - Clear the screen (k)
+            //  - Increase the kernel log level (+)
+            //  - Decrease the kernel log level (-)
 
         case KEY_ESCAPE:
             // Exit the OS if we press escape three times in a row
@@ -191,18 +220,16 @@ void kernel_command(char c) {
             break;
     }
 }
-
-
-/**
- * Exits the kernel
- */
-void kernel_exit(void) {
+//d
+void kernel_exit(void) { //f TODO
+    /**
+     * Exits the kernel
+     */
     // Print to the terminal
     printf("Exiting %s...\n", OS_NAME);
-
-    // Print to the VGA display
-
+    // Print to the VGA display TODO
     // Exit
     exit(0);
 }
+//d
 
