9d4e972b2a59f4a145a20b4c66503aa06c71db64
Sanjot chandi <schandi@csus.edu>
2024-04-23 00:57:47 -0700

    Rolled back Kernel.h due to build issues

--- include/kernel.h
+++ include/kernel.h
@@ -7,16 +7,16 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#define KSTACK_SIZE 16384   // Kernel Stack Size
-#define KCODE_SEG   0x08    // Kernel Code Segment
-#define KDATA_SEG   0x10    // Kernel Data Segment
+#define KSTACK_SIZE 16384
+#define KCODE_SEG 0x08
+#define KDATA_SEG 0x10
 
 #ifndef ASSEMBLER
-#include <spede/machine/asmacros.h>
 #include "kproc.h"
+#include <spede/machine/asmacros.h>
 
 #ifndef OS_NAME
-#define OS_NAME "Cabal"
+#define OS_NAME "MyOS"
 #endif
 
 // List of kernel log levels in order of severity
@@ -30,7 +30,7 @@ typedef enum log_level {
     KERNEL_LOG_LEVEL_ALL    // Log everything!
 } log_level_t;
 
-// Global pointer to the current active process entry
+//global pointer to the current active process.
 extern proc_t *active_proc;
 
 /**
@@ -106,31 +106,24 @@ int kernel_get_log_level(void);
  * @param level - the log level to set
  * @return the kernel log level
  */
-int kernel_set_log_level(int level);
+int kernel_set_log_level(log_level_t level);
+
+/**
+ * Triggers a breakpoint (if running under GDB)
+ */
+void kernel_break(void);
 
 /**
  * Exits the kernel
  */
 void kernel_exit(void);
 
-/**
- * Kernel entrypoint
- *
- * This is the primary entry point for the kernel. It is only
- * entered when an interrupt occurs. When it is entered, the
- * process context must be saved. Any kernel processing (such as
- * interrupt handling) is performed before finally exiting the
- * kernel context to restore the proces state.
- */
 void kernel_context_enter(trapframe_t *trapframe);
 
-/* The following functions are written directly in assembly */
+//Following functions are written irectly in assembly.
 __BEGIN_DECLS
-/**
- * Exits the kernel context and restores the process context
- */
+//Exits the kernel context, restoring the process.
 extern void kernel_context_exit();
 __END_DECLS
-
 #endif
 #endif
